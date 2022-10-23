523a39976f2bc39327b83a18b0fd177538fa890a
Ivan Muhammad Nizar <ivannizar@gmail.com>
2022-10-23 09:08:38 +0700

    add getenv

--- controllers/akses_controller.py
+++ controllers/akses_controller.py
@@ -4,6 +4,13 @@ from models.akses_model import akses_model
 from models.auth_model import auth_model
 import jwt
 from configs.config import key_jwt
+import os
+
+config = {}
+config['key_jwt'] = os.getenv('key_jwt')
+
+if config['key_jwt'] == None:
+    config['key_jwt'] = key_jwt['key']
 
 obj = akses_model()
 auth = auth_model()
@@ -12,7 +19,7 @@ auth = auth_model()
 def add_akses():
     authorization = request.headers.get("authorization")
     token = authorization.split(" ")[1]
-    tokendata = jwt.decode(token, key_jwt["key"], algorithms="HS256")
+    tokendata = jwt.decode(token, config['key_jwt'], algorithms="HS256")
 
     data = {
         "user_id": request.form['user_id'],
--- models/dokumen_model.py
+++ models/dokumen_model.py
@@ -2,9 +2,29 @@ import mysql.connector
 from configs.config import dbconfig
 from flask import make_response
 
+import os
+
+config = {}
+config['host'] = os.getenv('db_host')
+config['username'] = os.getenv('db_username')
+config['password'] = os.getenv('db_hpassword')
+config['database'] = os.getenv('db_database')
+
+if config['host'] == None:
+    config['host'] = dbconfig['host']
+
+if config['username'] == None:
+    config['username'] = dbconfig['username']
+
+if config['password'] == None:
+    config['password'] = dbconfig['password']
+
+if config['database'] == None:
+    config['database'] = dbconfig['database']
+
 class dokumen_model():
     def __init__(self):
-        self.con = mysql.connector.connect(host=dbconfig['host'],user=dbconfig['username'],password=dbconfig['password'],database=dbconfig['database'])
+        self.con = mysql.connector.connect(host=config['host'],user=config['username'],password=config['password'],database=config['database'])
         self.con.autocommit=True
         self.cur = self.con.cursor(dictionary=True)
 
